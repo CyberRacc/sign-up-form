cbbbe5a6b2f37a9bcd0c4f129f252802f529d9f1
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2023-07-30 16:52:37 +0100

    Fixed layour of create account button so it stays in its correct location when resizing the window.

--- js/validate-passwd.js
+++ js/validate-passwd.js
@@ -1,3 +1,13 @@
-let validation = function() {
-    
-}
+console.log("Script Loaded")
+
+const form = document.querySelector("form");
+
+const passwordInput = document.querySelector("#password")
+const passwordConfirm = document.querySelector("#password-confirm")
+
+form.addEventListener("submit", (e) => {
+    if (passwordInput.value !== passwordConfirm.value) {
+        e.preventDefault();
+        alert("The passwords do not match. Please try again.");
+    }
+});
